1a873c122d14bd7972ca20914b37efc941966f1a
Yurii Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-09-10 15:19:26 +0300

    CommandLineRunner

--- lab1/MySpringConsoleHelloWorld/src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
+++ lab1/MySpringConsoleHelloWorld/src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
@@ -1,13 +1,20 @@
 package com.example.myspringconsolehelloworld;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySpringConsoleHelloWorldApplication {
+public class MySpringConsoleHelloWorldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        System.out.println("Start of main");
         SpringApplication.run(MySpringConsoleHelloWorldApplication.class, args);
+        System.out.println("End of main");
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello from Spring Boot!");
+    }
 }
