1d190f0d7522c4a7821bd85bae81c625991c70eb
Yurii Sahanovskyi <yuriisahanovskyi@gmail.com>
2024-09-10 15:13:16 +0300

    Console project created

---
+++ lab1/MySpringConsoleHelloWorld/src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringconsolehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringConsoleHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringConsoleHelloWorldApplication.class, args);
+    }
+
+}
