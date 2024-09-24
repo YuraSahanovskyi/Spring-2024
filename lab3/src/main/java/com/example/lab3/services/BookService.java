9a20256801782255ce80ed17324551a7497cb329
L1tosh <dan.litosh33@gmail.com>
2024-09-24 23:41:11 +0300

    Add book validation

--- lab3/src/main/java/com/example/lab3/services/BookService.java
+++ lab3/src/main/java/com/example/lab3/services/BookService.java
@@ -2,47 +2,65 @@ package com.example.lab3.services;
 
 import com.example.lab3.models.Book;
 import com.example.lab3.repositories.BookRepository;
+import com.example.lab3.utils.BookValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Collection;
 
 @Service
 @Scope("prototype")
 public class BookService {
     @Autowired
-    private  BookRepository bookRepository;
+    private BookRepository bookRepository;
+
+    private BookValidator bookValidator;
+
+    @Autowired
+    public void setBookValidator(BookValidator bookValidator) {
+        this.bookValidator = bookValidator;
+    }
 
     public Collection<Book> getAllBooks() {
         return bookRepository.findAll();
     }
+
     public Collection<Book> findByName(String name) {
         return bookRepository.findAll().stream()
-               .filter(book -> book.getName().toLowerCase().contains(name.toLowerCase()))
-               .toList();
+                .filter(book -> book.getName().toLowerCase().contains(name.toLowerCase()))
+                .toList();
     }
+
     public Collection<Book> findByAuthor(String author) {
         return bookRepository.findAll().stream()
                 .filter(book -> book.getAuthor().toLowerCase().contains(author.toLowerCase()))
                 .toList();
     }
+
     public Collection<Book> findByKeyword(String keyword) {
         return bookRepository.findAll().stream()
                 .filter(book -> book.getKeywords().contains(keyword.toLowerCase()))
                 .toList();
     }
+
     public boolean create(Book book) {
-        if (book != null){
-            return bookRepository.save(book);
-        } else {
+        if (!bookValidator.isValid(book))
             return false;
-        }
+
+        return bookRepository.save(book);
     }
+
     public boolean delete(int id) {
-           return bookRepository.remove(id);
+        return bookRepository.remove(id);
     }
+
     public boolean update(Book book) {
+        if (!bookValidator.isValid(book))
+            return false;
+
         return bookRepository.modify(book);
     }
+
+
 }
