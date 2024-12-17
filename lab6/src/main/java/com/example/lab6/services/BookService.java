b43146e419b169fe8968f8ed4b5bbb5f52b09355
YuraSahanovskyi <yuriisahanovskyi@gmail.com>
2024-12-17 16:04:29 +0200

    Transactional

--- lab6/src/main/java/com/example/lab6/services/BookService.java
+++ lab6/src/main/java/com/example/lab6/services/BookService.java
@@ -6,6 +6,9 @@ import com.example.lab6.utils.BookValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -21,24 +24,28 @@ public class BookService {
         this.bookValidator = bookValidator;
     }
 
+    @Transactional(readOnly = true)
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Book get(int id){
         return bookRepository.getBookById(id);
     }
 
 
-
+    @Transactional(readOnly = true)
     public List<Book> findByAuthor(String author){
         return bookRepository.findByAuthor(author);
     }
 
+    @Transactional(readOnly = true)
     public List<Book> findByKeyword(String keyword) {
         return bookRepository.findByKeyword(keyword);
     }
 
+    @Transactional(propagation = Propagation.NESTED)
     public Book create(Book book) {
         if (!bookValidator.isValid(book))
             return null;
@@ -46,10 +53,12 @@ public class BookService {
         return bookRepository.save(book);
     }
 
+    @Transactional
     public void delete(int id) {
          bookRepository.deleteById(id);
     }
 
+    @Transactional(propagation = Propagation.NESTED)
     public Book update(Book book) {
         if (!bookValidator.isValid(book))
             return null;
